4ede67b9e447e94d6fc13074d6dd7fce97a9ffee
Balazs Juhasz <juhaszbal@gmail.com>
2020-12-20 17:31:51 +0100

    fix titles, save contact in files

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import csv
 app = Flask(__name__)
 print(__name__)
 
@@ -13,26 +14,29 @@ def html_page(page_name):
     return render_template(page_name)
 
 
-@app.route('/components.html')
-def components():
-    return render_template('components.html')
+def write_to_file(data):
+    with open('./database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        database.write(f'\n{email},{subject},{message}')
 
 
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
+def write_to_csv(data):
+    with open('./database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(
+            database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
 
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/work.html')
-def work():
-    return render_template('work.html')
-
-
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('./thankyou.html')
+    else:
+        return 'something went wrong. Try again!'
